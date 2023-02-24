bb1e69fb7e7ec4177411e1f5f7eee7aa3cb1efc7
rei-yo <yoshida.rei.755@gmail.com>
2023-02-24 10:41:48 +0900

    firstcommit

---
+++ django/startup_bbs/bbs/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Topic(models.Model):
+    comment     = models.CharField(verbose_name="コメント",max_length=2000)
+
+    def __str__(self):
+        return self.comment
+
+# Create your models here.
